fa3469bae826b6b06010a2528b080b5054debfd4
Jakub Czaplicki <jakub.czaplicki@gmail.com>
2011-10-11 21:58:37 +0100

    moved sensor reading to a separate class, preparing to merge jakub_sqlite branch

---
+++ iTesaApp/src/com/funellites/iTesa/Magnetometer.java
@@ -0,0 +1,52 @@
+package com.funellites.iTesa;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class Magnetometer {
+	private SensorManager sensorManager = null;
+    private Magnetometer.Callback cb = null;
+    private int i = 0;
+    
+    public Magnetometer(Context context,Magnetometer.Callback cb) {
+    	this.cb=cb;
+
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener( sensorEventListener,
+        		sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+        		SensorManager.SENSOR_DELAY_GAME);
+    }
+    
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+    	
+        public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+        public void onSensorChanged(SensorEvent event) {
+        	synchronized (this) {
+        		switch (event.sensor.getType()){ 
+        		case Sensor.TYPE_MAGNETIC_FIELD:
+        			i++;
+        			cb.updateData(event.timestamp,
+        					      event.values[0], 
+        					      event.values[1],
+        					      event.values[2],
+        					      i );
+	            	break;
+                }
+        	}
+        }
+    };
+    
+    public void close() {
+    	sensorManager.unregisterListener(sensorEventListener,
+        		sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
+    }
+
+    public interface Callback {
+        void updateData(long t, float x,float y,float z, int i);
+    }
+
+}
--- iTesaApp/src/com/funellites/iTesa/MainActivity.java
+++ iTesaApp/src/com/funellites/iTesa/MainActivity.java
@@ -4,49 +4,43 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+public class MainActivity extends Activity implements Magnetometer.Callback {
 
-public class MainActivity extends Activity {
-
-	SensorManager sensorManager = null;
 	protected TextView tBTextView;
 	protected TextView xBTextView;
 	protected TextView yBTextView;
 	protected TextView zBTextView;
 	protected TextView absBTextView;
     protected TextView maxBTextView;
-    TextView outMFN;
+    protected TextView iTextView;
+
+    float xB,yB,zB; // TODO: move to DataItem constructor
+    long t;
+    DataItem B = new DataItem(t, xB, yB, zB);
     float absB = 0;
     float maxB = 0;
-    float xB,yB,zB;
-    long  t;
-    // DBAdapter dbAdapter; // FIXME
-
-	
+    
+    Magnetometer magnetometer = null;
+    
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        /* Open Database */
-        /* FIXME:
-        dbAdapter = new DBAdapter(this);
-        dbAdapter.open();
-        */
         tBTextView = (TextView) findViewById(R.id.tB);
         xBTextView = (TextView) findViewById(R.id.xB);
         yBTextView = (TextView) findViewById(R.id.yB);
         zBTextView = (TextView) findViewById(R.id.zB);
         absBTextView = (TextView) findViewById(R.id.absB);
         maxBTextView = (TextView) findViewById(R.id.maxB);
-
+        iTextView = (TextView) findViewById(R.id.i);
+        
+        magnetometer = new Magnetometer( this , this );
+        
         Timer updateTimer = new Timer("bUpdate");
         updateTimer.scheduleAtFixedRate(new TimerTask() {
         	public void run() {
@@ -59,50 +53,27 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        /*sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener( sensorEventListener,
         		sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-        		sensorManager.SENSOR_DELAY_GAME);
+        		sensorManager.SENSOR_DELAY_GAME);*/
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        sensorManager.unregisterListener( sensorEventListener,
-        		sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
     }
-
-    private final SensorEventListener sensorEventListener = new SensorEventListener() {
-    	
-        public void onAccuracyChanged(Sensor sensor, int accuracy) { }
-
-        public void onSensorChanged(SensorEvent event) {
-        	synchronized (this) {
-        		switch (event.sensor.getType()){ 
-        		case Sensor.TYPE_MAGNETIC_FIELD:
-	            	xB = event.values[0]; //lateral
-	            	yB = event.values[1]; //longitudinal
-	            	zB = event.values[2]; //vertical
-	            	t  = event.timestamp;
-	            	absB = Math.round( Math.sqrt(xB*xB+yB*yB+zB*zB) ); 
-	            	if (absB > maxB)
-	            		maxB = absB;
-	            	break;
-	            	}
-        	}
-        }
-    };
-
+    
 	private void updateGUI() {
       runOnUiThread(new Runnable() {
     	  public void run() {
-    		  String str = "t: " + t;
+    		  String str = "t: " + B.t;
     		  tBTextView.setText(str);
-    		  str = "x: " + xB + " µT";
+    		  str = "x: " + B.x + " µT";
     		  xBTextView.setText(str);
-    		  str = "y: " + yB + " µT";
+    		  str = "y: " + B.y + " µT";
     		  yBTextView.setText(str);
-    		  str = "z: " + zB + " µT";
+    		  str = "z: " + B.z + " µT";
     		  zBTextView.setText(str);
     		  str = "abs: " + absB + " µT";
     		  absBTextView.setText(str);
@@ -115,12 +86,21 @@ public class MainActivity extends Activity {
     		  zBTextView.invalidate();
     		  absBTextView.invalidate();
     		  maxBTextView.invalidate();
-    		  
-    	      /* add to DB */
-    	      // FIXME: shit ain't workin'
-    		  /* DataItem newItem = new DataItem(xB, yB, zB, t);
-    	      long row = DBAdapter.insertData(newItem); */
     	  }
       });
-	}	
+	}
+	@Override
+	public void updateData(long time, float x, float y, float z, int i) {
+		B.t = time; // timestamp;
+	    B.x = x;    // lateral
+	    B.y = y;    // longitudinal
+	    B.z = z;    // vertical
+	    absB = Math.round( Math.sqrt(xB*xB+yB*yB+zB*zB) ); 
+	    if (absB > maxB)
+            maxB = absB;
+	    
+        String str = "i: " + i;
+        iTextView.setText(str);
+    }
+
 }
